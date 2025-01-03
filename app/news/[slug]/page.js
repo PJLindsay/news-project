8430fcc280620f0afcb274c1ed57eb194424c46e
PJ Lindsay <pjlindsay@gmail.com>
2025-01-02 19:14:17 -0700

    Create ability to click image (nested route inside dynamic route)

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,5 +1,8 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import { notFound } from "next/navigation";
+
+import Link from "next/link";
+
+import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function NewsDetailPage({ params }) {
   const newsSlug = params.slug;
@@ -12,7 +15,10 @@ export default function NewsDetailPage({ params }) {
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
+
         <h1>{newsItem.title}</h1>
         <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
