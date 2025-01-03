2d53c14a3fe20480b59c0cc6a42e64fc39b48498
PJ Lindsay <pjlindsay@gmail.com>
2025-01-02 17:25:52 -0700

    Add not found page for slug based news articles

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsArticleNotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Not Found!</h1>
+      <p>The requested news article could not be found.</p>
+    </div>
+  );
+}
