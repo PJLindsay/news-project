e5d4563a7ef8040bfd956f294b4ab091123716c4
PJ Lindsay <pjlindsay@gmail.com>
2025-01-02 17:02:20 -0700

    Add initial news landing page and sample news links

---
+++ app/news/page.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function NewsPage() {
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/news-1">News Item 1</Link>
+        </li>
+        <li>
+          <Link href="/news/news-2">News Item 2</Link>
+        </li>
+        <li>
+          <Link href="/news/news-3">News Item 3</Link>
+        </li>
+      </ul>
+    </>
+  );
+}
