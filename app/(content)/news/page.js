919c43984c88b5af59d677c75ae425b4ac0595f6
= <=>
2025-01-03 15:09:48 -0700

    Convert from client-side fetching to server-side fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,51 +1,17 @@
-"use client";
-
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState();
-  const [news, setNews] = useState();
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
 
-  useEffect(() => {
-    setIsLoading(true);
-    // you can call these async functions outside of the useEffect
-    async function fetchNews() {
-      const response = await fetch("http://localhost:8080/news");
-
-      if (!response.ok) {
-        setError("Failed to fetch news.");
-        setIsLoading(false);
-      }
-
-      console.log("reponse ==> ", response);
-      const news = await response.json();
-      setNews(news);
-      setIsLoading(false);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if (!response.ok) {
+    throw new Error("Failed to fetch news.");
   }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  let newsContent;
-
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  const news = await response.json();
 
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 }
