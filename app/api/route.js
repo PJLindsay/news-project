0ee5f588553345385161dd7e9e7da4e48356eef0
PJ Lindsay <pjlindsay@gmail.com>
2025-01-02 20:24:49 -0700

    Add in commented code

---
+++ app/api/route.js
@@ -0,0 +1,8 @@
+// functions must be named to match HTTP verbs (GET, PUT, POST, PATCH, DELETE)
+// export function GET(request) {
+//   console.log(request);
+
+//   // return Response.json();
+//   return new Response("Hello!");
+// }
+
---
+++ middleware.js
@@ -0,0 +1,16 @@
+// import { NextResponse } from "next/server";
+
+// // you can inspect/modify/redirect
+// export function middleware(request) {
+//   console.log("request ==> ", request);
+
+//   // forwards incoming request to it's actual destination
+//   return NextResponse.next();
+
+//   // return NextResponse.redirect();
+// }
+
+// // you can use this to filter for specific types of requests
+// export const config = {
+//   matcher: "/news"
+// }
